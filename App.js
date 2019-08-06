9938002cbfd8283dffd56c9b15dab754bf34ea0b
Aman Mittal <amandeepmittal@live.com>
2019-08-06 17:43:23 +0530

    complete

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Ionicon } from '@expo/vector-icons'
+import { StyleSheet, TouchableOpacity, View, Image, Text } from 'react-native'
+import { Ionicons } from '@expo/vector-icons'
 import { Audio } from 'expo-av'
 
 const audioBookPlaylist = [
@@ -8,46 +8,173 @@ const audioBookPlaylist = [
 		title: 'Hamlet - Act I',
 		author: 'William Shakespeare',
 		source: 'Librivox',
-		uri: 'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act1_shakespeare.mp3'
+		uri:
+			'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act1_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
 	},
 	{
 		title: 'Hamlet - Act II',
 		author: 'William Shakespeare',
 		source: 'Librivox',
-		uri: 'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act2_shakespeare.mp3'
+		uri:
+			'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act2_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
 	},
 	{
 		title: 'Hamlet - Act III',
 		author: 'William Shakespeare',
 		source: 'Librivox',
-		uri: 'http://www.archive.org/download/hamlet_0911_librivox/hamlet_act3_shakespeare.mp3'
+		uri: 'http://www.archive.org/download/hamlet_0911_librivox/hamlet_act3_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
 	},
 	{
 		title: 'Hamlet - Act IV',
 		author: 'William Shakespeare',
 		source: 'Librivox',
-		uri: 'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act4_shakespeare.mp3'
+		uri:
+			'https://ia800204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act4_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
 	},
 	{
 		title: 'Hamlet - Act V',
 		author: 'William Shakespeare',
 		source: 'Librivox',
-		uri: 'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3'
+		uri:
+			'https://ia600204.us.archive.org/11/items/hamlet_0911_librivox/hamlet_act5_shakespeare.mp3',
+		imageSource: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg'
 	}
 ]
 
 export default class App extends React.Component {
 	state = {
 		isPlaying: false,
-		track: null,
+		playbackInstance: null,
 		currentIndex: 0,
 		volume: 1.0,
-		isBuffering: false
+		isBuffering: true
 	}
+
+	async componentDidMount() {
+		try {
+			await Audio.setAudioModeAsync({
+				allowsRecordingIOS: false,
+				interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DO_NOT_MIX,
+				playsInSilentModeIOS: true,
+				interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DO_NOT_MIX,
+				shouldDuckAndroid: true,
+				staysActiveInBackground: true,
+				playThroughEarpieceAndroid: true
+			})
+
+			this.loadAudio()
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	async loadAudio() {
+		const { currentIndex, isPlaying, volume } = this.state
+
+		try {
+			const playbackInstance = new Audio.Sound()
+			const source = {
+				uri: audioBookPlaylist[currentIndex].uri
+			}
+
+			const status = {
+				shouldPlay: isPlaying,
+				volume: volume
+			}
+
+			playbackInstance.setOnPlaybackStatusUpdate(this.onPlaybackStatusUpdate)
+			await playbackInstance.loadAsync(source, status, false)
+			this.setState({
+				playbackInstance
+			})
+		} catch (e) {
+			console.log(e)
+		}
+	}
+
+	onPlaybackStatusUpdate = status => {
+		this.setState({
+			isBuffering: status.isBuffering
+		})
+	}
+
+	handlePlayPause = async () => {
+		const { isPlaying, playbackInstance } = this.state
+		isPlaying ? await playbackInstance.pauseAsync() : await playbackInstance.playAsync()
+
+		this.setState({
+			isPlaying: !isPlaying
+		})
+	}
+
+	handlePreviousTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			currentIndex < audioBookPlaylist.length - 1 ? (currentIndex -= 1) : (currentIndex = 0)
+			this.setState({
+				currentIndex
+			})
+			this.loadAudio()
+		}
+	}
+
+	handleNextTrack = async () => {
+		let { playbackInstance, currentIndex } = this.state
+		if (playbackInstance) {
+			await playbackInstance.unloadAsync()
+			currentIndex < audioBookPlaylist.length - 1 ? (currentIndex += 1) : (currentIndex = 0)
+			this.setState({
+				currentIndex
+			})
+			this.loadAudio()
+		}
+	}
+
+	renderFileInfo() {
+		const { playbackInstance, currentIndex } = this.state
+		return playbackInstance ? (
+			<View style={styles.trackInfo}>
+				<Text style={[styles.trackInfoText, styles.largeText]}>
+					{audioBookPlaylist[currentIndex].title}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{audioBookPlaylist[currentIndex].author}
+				</Text>
+				<Text style={[styles.trackInfoText, styles.smallText]}>
+					{audioBookPlaylist[currentIndex].source}
+				</Text>
+			</View>
+		) : null
+	}
+
 	render() {
 		return (
 			<View style={styles.container}>
-				<Text>Open up App.js to start working on your app!</Text>
+				<Image
+					style={styles.albumCover}
+					source={{ uri: 'http://www.archive.org/download/LibrivoxCdCoverArt8/hamlet_1104.jpg' }}
+				/>
+				<View style={styles.controls}>
+					<TouchableOpacity style={styles.control} onPress={this.handlePreviousTrack}>
+						<Ionicons name='ios-skip-backward' size={48} color='#444' />
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handlePlayPause}>
+						{this.state.isPlaying ? (
+							<Ionicons name='ios-pause' size={48} color='#444' />
+						) : (
+							<Ionicons name='ios-play-circle' size={48} color='#444' />
+						)}
+					</TouchableOpacity>
+					<TouchableOpacity style={styles.control} onPress={this.handleNextTrack}>
+						<Ionicons name='ios-skip-forward' size={48} color='#444' />
+					</TouchableOpacity>
+				</View>
+				{this.renderFileInfo()}
 			</View>
 		)
 	}
@@ -59,5 +186,31 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center'
+	},
+	albumCover: {
+		width: 250,
+		height: 250
+	},
+	trackInfo: {
+		padding: 40,
+		backgroundColor: '#fff'
+	},
+
+	trackInfoText: {
+		textAlign: 'center',
+		flexWrap: 'wrap',
+		color: '#550088'
+	},
+	largeText: {
+		fontSize: 22
+	},
+	smallText: {
+		fontSize: 16
+	},
+	control: {
+		margin: 20
+	},
+	controls: {
+		flexDirection: 'row'
 	}
 })
