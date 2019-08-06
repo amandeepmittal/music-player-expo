bfc876a8b08117ed2a740fa9d4537d2540b790fb
Aman Mittal <amandeepmittal@live.com>
2019-08-06 13:51:48 +0530

    install expo-av

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import { Ionicon } from '@expo/vector-icons'
+import { Audio } from 'expo-av'
 
 const audioBookPlaylist = [
 	{
